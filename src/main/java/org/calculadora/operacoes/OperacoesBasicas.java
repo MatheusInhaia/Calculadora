36005414ba980ff77c3adf07c67cd0be7d13b38b
MatheusInhaia <matheus.inhaia2014@gmail.com>
2024-02-23 10:24:47 -0300

    Criando as operações básicas.

--- src/main/java/org/calculadora/operacoes/OperacoesBasicas.java
+++ src/main/java/org/calculadora/operacoes/OperacoesBasicas.java
@@ -1,2 +1,25 @@
-package org.calculadora.operacoes;public class OperacoesBasicas {
+package org.calculadora.operacoes;
+
+public final class OperacoesBasicas {
+
+    private OperacoesBasicas(){
+        throw new AssertionError();
+    }
+
+    public static Double adicao(Double numeroUm, Double numeroDois){
+        return numeroUm+numeroDois;
+    }
+
+    public static Double subtracao(Double numeroUm, Double numeroDois){
+        return numeroUm-numeroDois;
+    }
+
+    public static Double multiplicacao(Double numeroUm, Double numeroDois){
+        return numeroUm*numeroDois;
+    }
+
+    public static Double divisao(Double numeroUm, Double numeroDois){
+        return numeroUm/numeroDois;
+    }
+
 }
